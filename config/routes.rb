9b5b1819a3664faf9341fc503ca62f6d84df911b
Diego Linhares <diegomlinhares@gmail.com>
2021-04-03 02:46:46 -0300

    Create discussions from the UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :discussions, only: [:index]
+  resources :discussions, only: [:index, :new, :create]
 
   root to: "main#index"
 end
