be2d84d5d68d179bd9875e35d710ecb675f14b4b
Diego Linhares <diegomlinhares@gmail.com>
2021-06-03 02:06:38 -0300

    Edit posts in realtime for everyone

--- app/controllers/discussions/posts_controller.rb
+++ app/controllers/discussions/posts_controller.rb
@@ -4,6 +4,7 @@ module Discussions
   class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_discussion
+    before_action :set_post, only: [:show, :edit, :update]
 
     def create
       @post = @discussion.posts.new(post_params)
@@ -18,7 +19,27 @@ module Discussions
       end
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post.discussion, notice: 'Post created' }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+      end
+    end
+
     private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
     def post_params
       params.require(:post).permit(:body)
