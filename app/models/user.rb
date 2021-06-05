2dcfa31b190622a1986ba8bd1b0c22e4fca20b5d
Diego Linhares <diegomlinhares@gmail.com>
2021-06-04 21:42:01 -0300

    Add discussion subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :discussion_subscriptions, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 
